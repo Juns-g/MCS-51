ba23f310cd186d00ed15e34d6402cfbbc24f621a
Juns <juns.g@foxmail.com>
2023-11-21 14:08:43 +0800

    first commit

---
+++ 10.c
@@ -0,0 +1,24 @@
+// 应用 8255A 扩展单片机的 I/O 口，将 8255A 的 A 口设置为输出方式
+// B 口设置为输入方式，实现通过 B 口的开关控制 A 口的发光二极管亮/灭的功能
+
+#include <reg51.h>
+#include <absacc.h>
+#define add XBYTE[0x0000] // 定义片外首地址
+// A 口地址 0x7cff
+// B 口地址 0x7dff
+// 控制字地址 0x7fff
+void main()
+{
+    unsigned char port_data;
+    unsigned char xdata *pt; // 宏定义片外指针
+    pt = &add;               // pt 指向片外首地址
+    *(pt + 0x7fff) = 0x82;
+    // 控制字, 0x82 = 1000 0010, 设置A为输出方式，B为输入方式
+    while (1)
+    {
+        port_data = *(pt + 0x7dff);
+        // 读取B口数据, 输入到port_data里
+        *(pt + 0x7cff) = port_data;
+        // 将port_data的值输出到A口
+    }
+}
